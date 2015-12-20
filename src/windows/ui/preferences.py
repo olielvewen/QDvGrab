53a05d43a6ef2691ea0b6c36a8fe2507259d5809
Olivier Girard <olivier@openshot.org>
2015-12-20 17:50:30 +0100

    complete some even handlers and some modification of the ui

--- src/windows/ui/preferences.py
+++ src/windows/ui/preferences.py
@@ -53,51 +53,97 @@ class PreFerences(QDialog):
         self.connectActions()
         self.updateUi()
 
-    #===========================================================================
+    #===================================================================================================================
     def setupUi(self):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
-    #===========================================================================
+    #===================================================================================================================
     def connectActions(self):
-        #pass
+        """ connection of all events """
         self.ui.btncredits.clicked.connect(self.Credits)
         self.ui.btndvgrab.clicked.connect(self.dvgrabpath)
         self.ui.btntranscode.clicked.connect(self.transcodepath)
         self.ui.btnchoosefile.clicked.connect(self.outputpath)
         self.ui.cmblanguages.activated.connect(self.languages)
+        self.ui.rdbdvraw.toggled.connect(self.choosedvformat)
+        self.ui.rdbdv2.toggled.connect(self.choosedvformat)
+        self.ui.rdbdv.toggled.connect(self.choosedvformat)
+        self.ui.rdbhdv.toggled.connect(self.choosehdvformat)
+        self.ui.rdbmpeg2.toggled.connect(self.choosehdvformat)
+        self.ui.chknone.toggled.connect(self.chooseautomaticconversion)
+        self.ui.chkdvraw.toggled.connect(self.chooseautomaticconversion)
+        self.ui.chkdv2.toggled.connect(self.chooseautomaticconversion)
+        self.ui.chkhdv.toggled.connect(self.chooseautomaticconversion)
+        self.ui.chkdetection.toggled.connect(self.choosedetectionscene)
+        self.ui.chkautomatic.toggled.connect(self.choosedetectionscene)
+        self.ui.chkscene.toggled.connect(self.choosedetectionscene)
+        self.ui.spbscene.valueChanged.connect(self.choosedetectionscene)
+        self.ui.chkautomaticrecord.toggled.connect(self.captureextraparameters)
+        self.ui.chkmanualrecord.toggled.connect(self.captureextraparameters)
+        self.ui.lnehours.textChanged.connect(self.captureextraparameters)
+        self.ui.lneminutes.textChanged.connect(self.captureextraparameters)
+        self.ui.chkactivepreview.toggled.connect(self.runactivepreview)
 
-    #===========================================================================
+    #===================================================================================================================
     def updateUi(self):
         self.ui.lbltranscode.setEnabled(False)
         self.ui.lnetranscode.setEnabled(False)
         self.ui.btntranscode.setEnabled(False)
-        self.ui.chkactivepreview.isChecked(True)
+        self.ui.chkactivepreview.setVisible(True)
+        self.ui.chknone.setChecked(True)
+        self.ui.rdbdvraw.setChecked(True)
+        self.ui.rdbhdv.setChecked(True)
+        self.ui.chkdetection.setChecked(True)
+        self.ui.chkautomaticrecord.setChecked(True)
 
-
-    #===========================================================================
+    #===================================================================================================================
     def Credits(self):
         " run the Credits dialog "
         self.windo = KvIso()
         self.windo.exec_()
 
-    #===========================================================================
+    #===================================================================================================================
     def dvgrabpath(self):
         pass
 
-    #===========================================================================
+    #===================================================================================================================
     def transcodepath(self):
         pass
 
-    #===========================================================================
+    #===================================================================================================================
     def outputpath(self):
         pass
 
-    #===========================================================================
+    #===================================================================================================================
     def languages(self):
         pass
 
-    #===========================================================================
+    #===================================================================================================================
+    def choosedvformat(self):
+        pass
+
+    #===================================================================================================================
+    def choosehdvformat(self):
+        pass
+
+    #===================================================================================================================
+    def chooseautomaticconversion(self):
+        pass
+
+    #===================================================================================================================
+    def choosedetectionscene(self):
+        pass
+
+    #===================================================================================================================
+    def runactivepreview(self):
+        pass
+
+    #===================================================================================================================
+    def captureextraparameters(self):
+        pass
+
+    #===================================================================================================================
 
 
 if __name__ == "__main__":
