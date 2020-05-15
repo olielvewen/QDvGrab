4b79a20f66cc94b199dc610a7d5d1cd357a06d4f
olielvewen <olivier@openshot.org>
2020-05-15 18:19:32 +0200

    Clean the license according #1

--- qdvgrab/about.py
+++ qdvgrab/about.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
- @file
- @brief about.py - This file contains the QDvGrab About dialog (i.e About for QdvGrab Project)
- @author Olivier Girard <olivier@openshot.org>
+#
+# about.py - A About screen for QDvgrab
+# Copyright (c) 2019-2020 Olivier Girard <olivier@openshot.org>
+#
+# QDvgrab is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# QDvgrab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
- @section LICENSE
-
- Copyright (c) 2014-2016 QDvGrab Team. This file is part of
- QDvGrab (http://www.qdvgrab.org), an open-source project
- dedicated to delivering ha tiny and easy tool for dvgrab.
-
- QDvGrab is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- QDvGrab is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with QDVgrab.  If not, see <http://www.gnu.org/licenses/>.
- """
 # Used to run it
 import sys
 import os
