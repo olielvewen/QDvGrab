bc515756d7bce1fbfbfb2377616166d1a63df99d
olielvewen <olivier@openshot.org>
2020-06-08 18:10:58 +0200

    Add ts files for translations - bug #8

---
+++ qdvgrab/qdvgrab.de_De.ts
@@ -0,0 +1,239 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>PreFerences</name>
+    <message>
+        <location filename="preferences.py" line="63"/>
+        <source>QDvGrab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="56"/>
+        <source>Dvgrab is not installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="63"/>
+        <source>Transcode is not installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="72"/>
+        <source> Change languages here </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="73"/>
+        <source> Choose here another directory other than this one by default </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="75"/>
+        <source> Choose here another path other than this one by default if this one has failed </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="76"/>
+        <source> Set the Credits Screen </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="77"/>
+        <source> Select another format than this one by default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="78"/>
+        <source> Select no conversion when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="79"/>
+        <source> Select Raw conversion when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="80"/>
+        <source> Select Dv2 conversion when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="81"/>
+        <source> Select Dv conversion when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="82"/>
+        <source> Select only file when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="83"/>
+        <source> Set several files following hour and date when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="84"/>
+        <source> Creation of a scene when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="85"/>
+        <source>  Stop acquisition if not enought free space disk </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="86"/>
+        <source> Planning the time of acquisition </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="87"/>
+        <source> See in real time when acquisition is done </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="129"/>
+        <source>My Awesome Movie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="173"/>
+        <source>QdvGrab - Choose a different path </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="195"/>
+        <source>QdvGrab - Choose a different path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="211"/>
+        <source>QDvGab - Open a Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QdvGrab</name>
+    <message>
+        <location filename="qdvgrab.py" line="123"/>
+        <source> Choose the Dv Acquisition mode </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="124"/>
+        <source> Set the Camcorder Name </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="125"/>
+        <source> Set the free space disk available </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="126"/>
+        <source> Set the used space disk </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="127"/>
+        <source> Set the total space disk not and available </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="128"/>
+        <source> Choose the HDV Acquisition mode </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="129"/>
+        <source> Run the film acquisition </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="130"/>
+        <source> Set the Preferences Screen </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="131"/>
+        <source> Set the website Project Page </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="132"/>
+        <source> Exit the application </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="135"/>
+        <source>The camcorder name is </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="136"/>
+        <source>The free space disk available is of </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="137"/>
+        <source>The space disk used is of </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="138"/>
+        <source>The total space disk is of </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="139"/>
+        <source>Dv Acquisition mode is used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="140"/>
+        <source>HDV Acquisition mode is used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="141"/>
+        <source>The process is run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="142"/>
+        <source>Preferences Screen has been called</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="143"/>
+        <source>The web help oneline is opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="144"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="170"/>
+        <source>My Awesome Movie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="204"/>
+        <source>QdvGrab - Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="204"/>
+        <source>Unable to open the Wiki page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qdvgrab.py" line="250"/>
+        <source>QDvGrab - Choose a Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
